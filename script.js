62751834f52ec6f90668843a12515b67a261b831
maundu55 <marymaundu.mm@gmail.com>
2024-09-27 23:22:09 +0300

    add play/pause functions

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 const progress =document.getElementById("progress");
 const song =document.getElementById("song");
 const ctrlIcon =document.getElementById("ctrlIcon");
+
+song.onloadedmetadata = function(){
+    progress.max = song.duration;
+    progress.value = song.currentTime;
+
+}
+function playPause(){
+    if(ctrlIcon.classList.contains('fa-pause')){
+        song.pause();
+        ctrlIcon.classList.remove('fa-pause');
+        ctrlIcon.classList.add('fa-play');
+    }
+    else{
+        song.play();
+        ctrlIcon.classList.add('fa-pause');
+        ctrlIcon.classList.remove('fa-play');
+    }
+}
